0fc809f7312c83d703855b7221304c6bf41cd591
Ytalo <ytalocrz@gmail.com>
2021-08-11 15:35:17 -0400

    atividade de strings e arrays documentos html e js.

---
+++ semama1/strings-arrays/index.js
@@ -0,0 +1,47 @@
+//1
+
+//let array
+//console.log('a. ', array) // underfined
+
+//array = null
+//console.log('b. ', array) //null 
+
+//array = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12,  13]
+//console.log('c. ', array.length) //11
+
+//let i = 0
+//console.log('d. ', array[i]) //2
+
+//array[i+1] = 19
+///console.log('e. ', array)//não intendi, mas no console ficou "[3, 19, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+
+//const valor = array[i+6]
+//console.log('f. ', valor) //não entendi, mas deu 9. 
+
+//2
+
+//const frase = prompt("Digite uma frase")
+
+//console.log(frase.toUpperCase().replaceAll("A", "I"), frase.length) //SUBI NUM ONIBUS EM MARROCOS 27
+
+// 1 - Exercício de escrita de cpd[igo
+
+//const nome = prompt("Qual é seu nome")
+//const seuEmail = prompt("Qual é seu e-mail")
+
+//console.log("O email " +seuEmail+ "foi cadastrado com sucesso. Seja bem-vindo(a), " + nome )
+
+//2 
+
+const comidasPreferidas = ["Macarroda, ","Frango Assado, ","Pizza, ","Carne Assada"]
+console.log(comidasPreferidas)
+console.log("Essa são minhas comidas preferidas")
+console.log(comidasPreferidas[0])
+console.log(comidasPreferidas[1])
+console.log(comidasPreferidas[2])
+console.log(comidasPreferidas[3])
+
+const qualSuaComida = prompt("Qual sua comida preferida")
+console.log(comidasPreferidas.push)
+
+
