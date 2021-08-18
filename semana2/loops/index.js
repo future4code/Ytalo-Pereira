c71d9fd43aac66d03345f5be0292d5a4d505d5f6
Ytalo Cruz <ytalocrz@gmail.com>
2021-08-18 15:45:12 -0400

    Lopps / Laçõs
    Atividade de loops

---
+++ semana2/loops/index.js
@@ -0,0 +1,86 @@
+//Exercícios de interpletação de código
+
+//1,
+
+//let valor = 0
+//for(let i = 0; i < 5; i++) {
+  //valor += i
+//}
+//console.log(valor)
+
+// eu nãp sei 
+
+//2.
+
+//const lista = [10, 11, 12, 15, 18, 19, 21, 23, 25, 27, 30]
+//for (let numero of lista) {
+  //if (numero > 18) {
+//		console.log(numero)
+	//}
+//}
+
+//Vai ser impresso a partir do 19 = [19, 21, 23, 25, 27, 30]
+//Acredito que não!
+
+//3.
+
+//const quantidadeTotal = Number(prompt("Digite a quantidade de linhas: "))
+//let quantidadeAtual = 0
+//while(quantidadeAtual < quantidadeTotal){
+  //let linha = ""
+  //for(let asteriscos = 0; asteriscos < quantidadeAtual + 1; asteriscos++){
+    //linha += "*"
+  //}
+  //console.log(linha)
+  //quantidadeAtual++
+//}
+
+// Vai imprimir 1 = *, 2 = **, 3 = ***, 4 = ****.  
+
+
+//exercício de escrita de código 
+
+
+        //const quantosBichinhos = Number (prompt("Quantos bichinhos de estimação você tem "))
+
+       
+        //if (quantosBichinhos === 0 ){
+          // console.log("Que pena, Você não pode adotar um pet!")
+          //} 
+
+          //const resultado = [];
+
+          //for (let e = 0; e < quantosBichinhos; e = e+1){
+
+           //if (quantosBichinhos === 0 ){
+               //    console.log("Que pena, Você não pode adotar um pet!")
+             //} else {
+                    //const digiteNomes = prompt("Digite os nomes dos bichinhos")  
+              //      resultado.push(digiteNomes);
+             //}
+            //  console.log(resultado)       
+          //}
+const arrayOriginal = [1, 2 ,3, 90, 45, 43, 43, 87, 42, 12, 43]
+
+function numeros (){
+    console.log(arrayOriginal)
+    return numeros
+}
+//console.log(arrayOriginal)
+
+
+function dez(){
+
+          var numeros = [1, 3, 4, 2000];
+var soma = numeros.reduce((a, b) => a + b);
+
+console.log(soma);
+          
+    
+}
+
+
+
+
+
+
