5a46c2c199d3808272d82af56885c70083c87400
Ytalo Cruz <ytalocrz@gmail.com>
2021-10-07 16:18:30 -0300

    Em andamento

--- semana9/Hook-useEffect/kook-useeffect/src/App.js
+++ semana9/Hook-useEffect/kook-useeffect/src/App.js
@@ -1,6 +1,29 @@
 import React from "react";
+import axios from "axios";
+import { useEffect, useState } from "react/cjs/react.development";
 
-const App = () => {
+
+const App = (props) => {
+ 
+  const [pokList, setPokList] = useState([])
+  const [pokeName, setPokeName] = useState("")
+
+   const ListaPokemon = (pokList) => {
+    axios.get("https://pokeapi.co/api/v2/pokemon/?limit=151").then(res => {
+      setPokList(res.data.results)
+    }).catch(err => {
+      alert(err)
+    })
+  }
+
+  // useEffect (() => {
+  //   setListaPokemon(props.pokeName)
+  // })
+
+  // const handlePokeName = (event) => {
+  //   Pokemon(event.target.value)
+  // }
+ 
   return (
     <div>
       OI
--- semana9/Hook-useEffect/kook-useeffect/src/components/PokeCard/PokeCard.js
+++ semana9/Hook-useEffect/kook-useeffect/src/components/PokeCard/PokeCard.js
@@ -1,9 +1,30 @@
-import React from "react";
+import axios from "axios";
+import React, {useEffect, useState} from "react";
 
 const PokeCards = () => {
+    const [pokemon, setPOkemon] = useState({})
+
+    const pegarPokemon = (pokeName) => {
+        axios.get(`https://pokeapi.co/api/v2/pokemon/${pokeName}`).then(res => {
+            setPOkemon(res.data)
+        }).catch(err => {
+            alert(err)
+        })
+    }
+
+    useEffect (() => {
+        pegarPokemon(props.pokemon)
+    }, [props.pokemon])
+
     return (
         <div>
-            OI
+            <h2>{pokemon.name}</h2>
+            <h2>{pokemon.weight}</h2>
+            {pokemon.types && <h2>{pokemon.types[0].type.name}</h2>}
+            {pokemon.sprites && (
+                <img  src={pokemon.sprites.front_default} alt={pokemon.name} />
+            )}
         </div>
     )
 }
+export default PokeCards
