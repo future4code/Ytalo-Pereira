24190d910d6726c0fd25b100c80021c9fe1345db
Ytalo Cruz <ytalocrz@gmail.com>
2021-10-14 22:05:50 -0300

    $ Autenticação em aplicações React - EM ANDAMENTO

--- semana10-projeto/Labex/labex/src/Telas/LoginPage.js
+++ semana10-projeto/Labex/labex/src/Telas/LoginPage.js
@@ -1,24 +1,70 @@
-import React from "react";
-import { useHistory } from "react-router-dom";
+import React, {useState} from "react";
+import { useHistory} from "react-router-dom";
+import axios from "axios";
 
 export function LoginPage () {
 
     const history = useHistory()
 
+    const [email, setEmail] = useState("");
+    const [senha, setSenha] = useState("");
+
     const Enviar = () => {
         history.push("/")
-    }
+    };
     
     const Entrar = () => {
         history.push("/admin/trips/list")
-    }
+    };
+    
+    const onChangeEmail = (ev) => {
+        setEmail(ev.target.value)
+    };
+
+    const onChangeSenha = (ev) => {
+        setSenha(ev.target.value)
+    };
+
+    const onSubmitLogin = () => {
+        const body ={
+            email: email,
+	        password: senha
+        }
+        axios.post("https://us-central1-labenu-apis.cloudfunctions.net/labeX/ytalo/login", body)
+        .then((res) => {
+           console.log('Deu certo', res.data)        
+        })
+        .catch((err) => {
+            console.log('Deu errado', err.response)
+
+        })
+    };
 
     return (
         <div>
-            LoginPage
+            <h1>LoginPage</h1>
+           
+           <main>
+               <input 
+               placeholder="Email"
+               type="Email"
+               value={email}
+               onChange={onChangeEmail}
+               />
+               <input 
+               placeholder="Senha"
+               type="Senha"
+               value={senha}
+               onChange={onChangeSenha}
+               />
+           </main>
 
+
+            <div>
             <button onClick={Enviar}>Voltar</button>
-            <button onClick={Entrar}>Entrar</button>
+            <button onClick={Entrar} onClick={onSubmitLogin}>Entrar</button>
+            </div>
+
         </div>
     )
 }
--- semana10-projeto/Labex/labex/src/Telas/TripDetailsPage.js
+++ semana10-projeto/Labex/labex/src/Telas/TripDetailsPage.js
@@ -1,9 +1,25 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect } from "react";
 
 export function TripDetailsPage () {
+
+   useEffect(() => {
+    axios.get('https://us-central1-labenu-apis.cloudfunctions.net/labeX/ytalo/trip/:F5Xq3C5qca7ffkn0eiaz', {
+        headers: {
+            auth:'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Ijc0S…zNDd9.2ahmoA2MX4clRR6bdq9lX976h_OO-iofzquoX8hfwpc'
+        }  
+      })
+      .then((res) => {
+          console.log('Deu certo', res.data)
+      }).catch((err) => {
+          console.log('Deu errado' , err.response)
+      })
+   }, [])
+
+
     return (
-        <div>TripDetailsPage
-            alert("Enviado com sucesso")
+        <div>Detalhe da Viagem
+           
         </div>
     )
 }
