13f38d235ee2fe1bfc055a227faa7e1e982ba8c8
nomytalo <email>
2021-12-14 23:04:58 -0300

    em andamento

---
+++ Intro-Autenticação/src/index.ts
@@ -0,0 +1,9 @@
+import app from "./app";
+import editUser from './endpoints/editUser';
+import createUser from './endpoints/createUser';
+
+app.post('/users/signup', createUser)
+app.put('/users/:id', editUser)
+
+
+
